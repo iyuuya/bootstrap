c893192861e0754fa7081305fea1319dcc3f9fa0
iyuuya <i.yuuya@gmail.com>
2023-12-29 15:41:37 +0900

    fix: install or update

--- common/dotfiles.zsh
+++ common/dotfiles.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
-if [ -d ~/.local/src/github.com/iyuuya/dotfiles ]; then
+if [ ! -d ~/.local/src/github.com/iyuuya/dotfiles ]; then
   git clone https://github.com/iyuuya/dotfiles ~/.local/src/github.com/iyuuya/dotfiles
+else
+  git -C ~/.local/src/github.com/iyuuya/dotfiles pull
 fi
